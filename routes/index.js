52ee1eaaf56282d45b0c8cf1a20710e05db98fdd
Sidor007 <Sidor007@yandex.ru>
2022-09-03 15:46:30 +0700

    appChange

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ const userRouter = require('./users');
 const movieRouter = require('./movies');
 const { NotFoundError } = require('../errors/NotFoundError');
 
-router.use('/api', userRouter);
-router.use('/api', movieRouter);
+router.use(userRouter);
+router.use(movieRouter);
 router.use(() => {
   throw new NotFoundError('Wrong URL');
 });
